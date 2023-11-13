06f48bd321d188a355563af49cadae0db980bdea
Riccardo <prt.riccardo.01@gmail.com>
2023-11-13 20:47:05 +0100

    final dataset eduFacilities

--- Phase 2 - Information Gathering/Code Libraries/EducationFacilities/university.py
+++ Phase 2 - Information Gathering/Code Libraries/EducationFacilities/university.py
@@ -4,7 +4,7 @@ import re
 
 typeEdu = 'university'
 url = 'https://webapps.unitn.it/du/it/StrutturaAccademica' 
-file_path = r'C:\Users\Riccardo\OneDrive\Magistrale\Semestre1\KnowledgeGraphEngineering\EducationFacilities\university.csv'
+file_path = '../../Raw data/edu/university.csv'
 
 response = requests.get(url) 
 response.raise_for_status()
@@ -18,29 +18,15 @@ departements = div.find_all('div', class_=re.compile(r'struttura dip-'))
 for department in departements:
     name = department.find('h2').text.strip() 
     address = department.find('div', class_='indirizzo').text
-
-    nominatim = "https://nominatim.openstreetmap.org/search"
-    params = {
-        "q": address,
-        "format": "json"
-    }
-
-    response1 = requests.get(nominatim, params=params)
-    data = response1.json()
-
-    if data:
-        first_result = data[0]
-        latitude = first_result["lat"]
-        longitude = first_result["lon"]
-    else:
-        latitude = '-'
-        longitude = '-'
-        
-    school_info = [typeEdu, name, address, latitude, longitude]
+    
+    school_info = [typeEdu, name, address] 
     school_infos.append(school_info)
 
-df_university = pd.DataFrame(school_infos, columns=['typeEdu', 'name', 'address','latitude', 'longitude'])
+df_university = pd.DataFrame(school_infos, columns=['type', 'name', 'address'])
 df_university = df_university.drop_duplicates(subset=['name', 'address'])  
+df_university['name'] = df_university['name'].str.replace(',', ';')
+df_university['address'] = df_university['address'].str.replace(',', '')
+df_university.insert(0, 'id', range(1, len(df_university) + 1))
 pd.set_option('display.max_colwidth', None)
 df_university.to_csv(file_path, index=False)
 #print(df_university)  
