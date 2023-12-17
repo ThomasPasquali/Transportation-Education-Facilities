bed853fd8c3cf1e1608c059b8a8a3dbe56571700
Thomas Pasquali <tom.pasqui00@gmail.com>
2023-12-17 15:49:11 +0100

    Connectivity matrix + legend

--- Phase 2 - Information Gathering/Code Libraries/parser/evaluation.py
+++ Phase 2 - Information Gathering/Code Libraries/parser/evaluation.py
@@ -7,6 +7,7 @@ from lib.utils import add_id_prefix, err, warn, info, positions_near_enough
 DFS = {}
 etypes = [e.value for e in ETYPE]
 conn_matrix = pd.DataFrame(columns=[e['name'] for e in etypes], index=[e['name'] for e in etypes])
+legend = pd.DataFrame(columns=['Etype - Etype', 'Property names'])
 
 for e in ETYPE:
   DFS[e.value['name']] = read_tmp_dataset(e)
@@ -20,19 +21,31 @@ for etype1 in ETYPE:
     etype2_v = etype2.value
     etype2_name = etype2_v['name']
 
-    value = np.NAN
+    res = []
+    tot = np.NAN
+    columns = []
+
     if etype1 == etype2:
-      value = df[etype1_v['columns']].isnull().sum().sum() / df[etype1_v['columns']].count().sum()
+      columns = etype1_v['columns']      
     else:
-      columns = []
       for i in range(len(etype1_v['relations'])):
         if etype2_name == etype1_v['relations_etype'][i]:
           columns.append(etype1_v['relations'][i])
-      print(etype1_name, etype2_name, columns)
-      if len(columns) > 0:
-        value = df[columns].isnull().sum().sum() / df[columns].count().sum()
 
-    conn_matrix[etype1_name][etype2_name] = value
+    tot = np.NAN
+    if len(columns) > 0:
+      tot = 0
+      for c in columns:
+        res.append(df[c].isnull().sum() / len(df[c]))
+        tot += df[c].isnull().sum()
+      tot /= len(columns) * len(df)
+
+    # if tot > 1:
+    #   print(f'{etype1_name} - {etype2_name} {columns}  {tot}')
+
+    legend.loc[len(legend)] = [f'{etype1_name} - {etype2_name}', ' | '.join(columns)]
+    conn_matrix[etype2_name][etype1_name] = ' | '.join(['{:.2f}'.format(v) for v in res]) + ' tot: ' + '{:.2f}'.format(tot) if not np.isnan(tot) else ''
   
-print(conn_matrix)
-conn_matrix.to_csv('../../../Phase 5 - Data Definition/connectivity_matrix.csv')
+# print(conn_matrix)
+conn_matrix.to_csv('../../../Phase 5 - Data Definition/connectivity_matrix.csv', index=True)
+legend.to_csv('../../../Phase 5 - Data Definition/connectivity_matrix_legend.csv', index=False)
